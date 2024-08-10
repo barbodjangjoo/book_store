c4d338feaeabce7c9f75b93d36c70b97590cad1a
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2024-08-10 16:26:10 +0330

    add signup and login feature to the website

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('login')
