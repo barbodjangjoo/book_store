74ec7b56a0af4aec2ad8278d152b40f47c8bf70a
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2024-08-11 17:34:23 +0330

    add book model to admin

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -11,4 +12,11 @@ class Book(models.Model):
 
     price = models.PositiveIntegerField()
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("book_detail", args={self.pk})
+    
 
