54b5733c9a2ab762ba141ce08a1ef9a5e608c21a
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2024-08-11 17:10:59 +0330

    implement books model

---
+++ books/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=300)
+    description = models.TextField()
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    price = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local app
-    'accounts'
+    'accounts',
+    'books',
 ]
 
 MIDDLEWARE = [
