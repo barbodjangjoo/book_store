a64744f38d0f12617ee7bb4c5791bf1859baacc3
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2024-08-18 11:33:33 +0330

    add mysql db to project

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','0.0.0.0']
 
 
 # Application definition
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'mysql', 
+        'PORT': '3306',
     }
 }
 
