4413d1b2ab659b04e27776ec054d8629b137846b
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2024-08-23 13:06:06 +0330

    add detailview for authors

--- authors/urls.py
+++ authors/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.author_list_view, name='author_list'),
+    path('<int:pk>/', views.author_detail_view, name='author_detail'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+
 from .models import Author
+
 
 def author_list_view(request):
     authors = Author.objects.all()
     return render(request, 'authors/author_list.html', {'authors': authors})
 
+def author_detail_view(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    return render(request, 'authors/author_detail.html', {'author': author})
